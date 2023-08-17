122dc31dcc78b1c2ad845fb90bb79e328729f399
Mayurwagh98 <mswagh1998@gmail.com>
2023-08-17 22:39:16 +0530

    Added: sum of two numbers basic program in ts

---
+++ sum_of_num/index.ts
@@ -0,0 +1,14 @@
+//1 - write a program to add 2 numbers
+//2 - TS congfiguration file --> tsc --init 
+// (we do TS congfiguration beacuse we don't want index.js to be created if there is any error in index.ts)
+// command to do TS condif --> tsc --noEmitOnError index..ts
+
+
+// @ts-ignore --> to ignore duplication func name error
+
+function sum(a:number,b:number):number{
+
+    return a + b
+}
+
+console.log(sum(5,5))
