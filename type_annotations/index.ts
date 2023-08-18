dcb515bb4f0bf9b269a6e6aa89d4635343536881
Mayurwagh98 <mswagh1998@gmail.com>
2023-08-18 06:02:26 +0530

    Added: new todo in type annotations

---
+++ type_annotations/index.ts
@@ -0,0 +1,14 @@
+// todo --> String initialization
+// declare a vairable message of type string and assign it the value "hello typscript"
+
+// todo --> Concatenation
+// Create 2 variables firstName and lastName of type string and assign them your first and lastname, concatenate
+// the 2 variables and store them in fullName vairbale
+
+// todo --> String length
+// Declare a vaiable sentence with string type and assign it a sentence of your choice, find the length
+// of that sentence and store it in sentenceLen variable
+
+// todo --> Uppercase and Lowercase
+// Decalre a variable name text, assign it a sentence of your choice and convert that sentence into
+// uppercase and lowercase store both of them in new variables with name, upperText and lowerText
