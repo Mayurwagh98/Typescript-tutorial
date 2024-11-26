a54554b8b6ec13836f26c81340eca1f4ee0dec8c
mayur wagh <mayurwagh@mayurs-Air.bwrouter>
2024-11-26 21:30:45 +0530

    Added: goo practice

--- 01intro/myvariable.ts
+++ 01intro/myvariable.ts
@@ -4,5 +4,7 @@ let myvariable:string = "mayur"
 // not a good practice as its pretty obvious that myNum is a number,ts is type interference
 let myNum:number = 2323
 
+// good pratice, if its obvious then no need to decalare type of variable
+let myNewNum = 23222
 
 console.log(myvariable)
