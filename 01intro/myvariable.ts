cea88bb813ea586819ad8c5ff27a7b3191cb4df1
mayur wagh <mayurwagh@mayurs-Air.bwrouter>
2024-11-26 21:29:16 +0530

    Added: bad practice

--- 01intro/myvariable.ts
+++ 01intro/myvariable.ts
@@ -1,4 +1,8 @@
 let myvariable:string = "mayur"
 
 
+// not a good practice as its pretty obvious that myNum is a number,ts is type interference
+let myNum:number = 2323
+
+
 console.log(myvariable)
