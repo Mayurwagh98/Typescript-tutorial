a4424950a2c600705e1bb7cc276c78c323205ca2
mayur wagh <mayurwagh@mayurs-Air.bwrouter>
2024-11-28 00:21:28 +0530

    Added: interface re-opening & inheritence

--- 01intro/myInterfaces.ts
+++ 01intro/myInterfaces.ts
@@ -8,10 +8,36 @@ interface Iuser {
     getCoupon(name:string, value:number):number
 }
 
+
+// re-oepening of interfaces (its nothing but adding few extra things in a interface)
+// like here i am adding gitHubToken in Iuser
+interface Iuser{
+    githubToken:string
+}
+
+// inheritance
+interface AdminUser extends Iuser {
+    role:"admin"| "ts" | "learner"
+}
+
 const newIuser:Iuser = {
     name:"mayur",
     email:"m@gmail.com",
     userId:123, 
+    githubToken:"token",
     startTrail:() => "something",
     getCoupon:(name:"mayur", off:20) => 20
 }
+
+// inherited the properties from Iuser
+const newIuser2:AdminUser = {
+    name:"mayur",
+    email:"m@gmail.com",
+    userId:123, 
+    githubToken:"token",
+    role:"admin",
+    startTrail:() => "something",
+    getCoupon:(name:"mayur", off:20) => 20
+}
+
+
