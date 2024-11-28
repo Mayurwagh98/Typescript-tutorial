db780911f76d544255ce206494040aef9edffa62
mayur wagh <mayurwagh@mayurs-Air.bwrouter>
2024-11-28 23:54:37 +0530

    Adding: private & public keyword

--- 02config/src/index.ts
+++ 02config/src/index.ts
@@ -1,13 +1,29 @@
-class User {
-    email:string
+// class User {
+//     email:string
+//     name:string
+//     city:string = "dhule"
+//     readonly lastname:string = "wagh"
+//     constructor(email:string, name:string){
+//         this.email= email,
+//         this.name = name
+//     }
+// }
+
+// let newUser = new User("mayur@gmail.com","mayur")
+// newUser.city = "nashik"
+
+// public and private keywords
+
+class User{
+    public email:string // we can mark email as public, but by default all the parameters are marked as public
     name:string
-    city:string = "dhule"
-    readonly lastname:string = "wagh"
-    constructor(email:string, name:string){
-        this.email= email,
+    private city:string = "mayur" // making city as private, it won't be accessible outside
+    constructor(email:string,name: string){
+        this.email = email
         this.name = name
+        this.city = "dhule" // as city is marked as private, but it will still be accessible in class, but not outside of it
     }
 }
 
 let newUser = new User("mayur@gmail.com","mayur")
-newUser.city = "nashik"
+// newUser.city -> gives an error
