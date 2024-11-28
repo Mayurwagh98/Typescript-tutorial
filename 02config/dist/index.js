db2e288a949757e2526d310beb190d9a07d52659
mayur wagh <mayurwagh@mayurs-Air.bwrouter>
2024-11-28 18:46:16 +0530

    Added: constructor

--- 02config/dist/index.js
+++ 02config/dist/index.js
@@ -1,2 +1,11 @@
 "use strict";
-console.log("hi this is mayur");
+class User {
+    constructor(email, name) {
+        this.city = "dhule";
+        this.lastname = "wagh";
+        this.email = email,
+            this.name = name;
+    }
+}
+let newUser = new User("mayur@gmail.com", "mayur");
+newUser.city = "nashik";
--- 02config/src/index.ts
+++ 02config/src/index.ts
@@ -1,1 +1,13 @@
-console.log("hi this is mayur")
+class User {
+    email:string
+    name:string
+    city:string = "dhule"
+    readonly lastname:string = "wagh"
+    constructor(email:string, name:string){
+        this.email= email,
+        this.name = name
+    }
+}
+
+let newUser = new User("mayur@gmail.com","mayur")
+newUser.city = "nashik"
