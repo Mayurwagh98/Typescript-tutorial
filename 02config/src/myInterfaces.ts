cb554e06d512ac1f23ee15099c48943746e10b20
mayur wagh <mayurwagh@mayurs-Air.bwrouter>
2024-12-04 21:22:24 +0530

    Added: interfacses

--- 02config/src/myInterfaces.ts
+++ 02config/src/myInterfaces.ts
@@ -1,19 +1,39 @@
 interface TakePhoto{
     cameraMode:string,
     filter:string,
-    burstMode:number
+    burstMode:number,
+}
+
+interface Story{
+    createStory(): void
 }
 
 class Instrgram implements TakePhoto{
     constructor(public cameraMode:string, public filter: string, public burstMode:number){}
 }
 
-class Youtube implements TakePhoto{
+// class Youtube implements TakePhoto{
+//     constructor(
+//         public cameraMode:string,
+//         public filter:string,
+//         public burstMode:number,
+//         public short:string
+//     ){}
+// }
+
+// we can use multiple calsses as well
+class Youtube implements TakePhoto, Story{
     constructor(
         public cameraMode:string,
         public filter:string,
         public burstMode:number,
         public short:string
     ){}
+
+    createStory(): void {
+        console.log("hey")        
+    }
 }
+
+
 
