af0519ee0502fb0884785c1ee3c0610d423c79f5
mayur wagh <mayurwagh@mayurs-Air.bwrouter>
2024-12-04 22:07:03 +0530

    Added: Type keyword

--- 02config/src/myGenerics.ts
+++ 02config/src/myGenerics.ts
@@ -5,3 +5,18 @@ function identityOne(val: string | number): string| number{
     return val
 }
 
+function identityTwo(val: any):any{
+    return val
+}
+
+// if we are not sure what type of value will be passing
+// we can use Type keyword
+function identityThree<Type>(val:Type):Type{
+    return val
+}
+
+// using keyword using Type is not necessary we can use
+// any keyword similar to Type for example we can use T or any other word or letter as show below
+function identityFour<T>(val:T):T{
+    return val
+}
