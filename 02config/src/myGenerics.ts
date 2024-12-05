9b1452b3eed32d73af6546711601c9620b99d244
mayur wagh <mayurwagh@mayurs-Air.bwrouter>
2024-12-05 22:15:13 +0530

    Added: more examples of generecis

--- 02config/src/myGenerics.ts
+++ 02config/src/myGenerics.ts
@@ -30,3 +30,15 @@ interface Bottle{
 function identityFive<Bottle>(val:Bottle):Bottle{
     return val
 }
+
+function getSearchProducts<T>(products:T[]):T{
+    // do some database operations
+    const myIndex = 3
+    return products[myIndex]
+}
+
+const getMoreSearchProducts = <T>(products:T[]):T =>{
+    // do some database operations
+    const myIndex = 4
+    return products[myIndex]
+}
