5f7c90f060e34f220b5f66cb57bff57339d6b365
mayur wagh <mayurwagh@mayurs-Air.bwrouter>
2024-12-18 23:31:40 +0530

    Added: dicrimated unions and never

--- 02config/src/detections.ts
+++ 02config/src/detections.ts
@@ -55,6 +55,7 @@ function logValue(x: Date | string) {
   }
 }
 
+// using type predicates
 type Fish = { swim: () => void };
 type Bird = { fly: () => void };
 
@@ -69,5 +70,48 @@ function getFood(pet: Fish | Bird) {
   } else {
     pet;
     return "bird food";
+  }
+}
+
+// discrimanted unions
+interface Circle {
+  kind: "circle";
+  radius: number;
+}
+
+interface Square {
+  kind: "square";
+  side: number;
+}
+
+interface Rectangle {
+  kind: "rectangle";
+  length: number;
+  width: number;
+}
+
+type Shape = Circle | Square | Rectangle;
+
+function geTrueShape(shape: Shape) {
+  if (shape.kind === "circle") {
+    return Math.PI * shape.radius ** 2;
+  }
+  // return shape.side * shape.side; => this will throw an error as side doesn;t exists on reactangle
+}
+
+function getArea(shape: Shape) {
+  switch (shape.kind) {
+    case "circle":
+      return Math.PI * shape.radius ** 2;
+
+    case "square":
+      return shape.side * shape.side;
+
+    case "rectangle":
+      return shape.length * shape.width;
+
+    default:
+      const _defaultforShape: never = shape;
+      return _defaultforShape;
   }
 }
