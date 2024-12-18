96711e869d518d2c5d5498358da783fd01de2c5d
mayur wagh <mayurwagh@mayurs-Air.bwrouter>
2024-12-18 22:06:53 +0530

    Added: few more examples

--- 02config/src/detections.ts
+++ 02config/src/detections.ts
@@ -5,3 +5,25 @@ function detectType(val: number | string) {
 
   return val + 3;
 }
+
+function provideId(id: string | null) {
+  if (!id) {
+    console.log("please provide an ID");
+    return;
+  }
+  id.toLowerCase();
+}
+
+function printAll(strs: string | string[] | null) {
+  // checking if type is null
+  if (strs) {
+    // checking if type is array
+    if (typeof strs === "object") {
+      for (const s of strs) {
+        console.log(s);
+      }
+    } else if (typeof strs === "string") {
+      console.log(strs);
+    }
+  }
+}
